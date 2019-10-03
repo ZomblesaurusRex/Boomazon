b78f0cf39d42ad0a557eb59adcfde44a859b251d
Christin Boner <christinmclemons@gmail.com>
2019-10-02 21:19:02 -0700

    adding final product of code

--- boom.js
+++ boom.js
@@ -1,5 +1,125 @@
 // require mySQL, inquirer
-
+var mysql = require("mysql");
+var inquirer = require("inquirer");
 // connection for mySQL
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "boomazon_DB"
+});
 
-//
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("Welcome Back!");
+    var sql = "SELECT * from inventory";
+    connection.query(sql, function (err, result) {
+        if (err) throw err;
+        // prints out current table in boomazon_DB
+        console.table(result);
+
+        start();
+    });
+});
+// start
+function againConfirm(){
+    // console.log("==================================================================");
+
+    inquirer.prompt([
+        {
+            name: "again",
+            type: "confirm",
+            message: "Would you like to make another purchase?"
+            
+        }
+    ]).then(function (answer) {
+        if (answer.again) {
+            console.log("==================================================");
+            start();
+        } else {
+            console.log("Thank you, have a nice day!");
+            connection.end();
+
+        }
+    })
+
+};
+// end
+function start() {
+    console.log("INSTRUCTIONS: Enter the ID number of the corresponding product you would like to purchase.");
+    console.log("============================================================================================");
+    // prints out current table in boomazon_DB
+
+    inquirer.prompt([
+        {
+            name: "productID",
+            type: "input",
+            message: "Which impulse purchase would you like to make today?",
+            validate: function (value) {
+                if (isNaN(value) === false) {
+                    // console.log(value);
+                    return true;
+                }
+                return false;
+            }
+        },
+        {
+            name: "productNumber",
+            type: "input",
+            message: "How many would you like to buy?",
+            validate: function (value) {
+                if (isNaN(value) === false) {
+                    // console.log(value);
+                    return true;
+                }
+                return false;
+            }
+        }
+    ]).then(function (answer) {
+        var productID;
+
+        var chosenItem;
+        var sql = "SELECT * from inventory";
+        connection.query(sql, function (err, results) {
+            if (err) throw err;
+
+            // console.log(answer.productID, answer.productNumber);
+            var chosenProductId = parseInt(answer.productID);
+            var chosenProductNumber = parseInt(answer.productNumber);
+
+            for (var i = 0; i < results.length; i++) {
+                // console.log(results[i].id);
+                // console.log(answer.productID);
+                // console.log(results[i].id === chosenProductId);
+                if (results[i].id === chosenProductId) {
+                    chosenItem = results[i];
+                    if (chosenProductNumber <= chosenItem.item_stock) {
+                        console.log("Thank you, Purchase Completed!");
+                        console.log("==============================================================");
+                        var sql2 = "UPDATE inventory SET item_stock = " + (chosenItem.item_stock - chosenProductNumber) + " WHERE id =" + chosenProductId + ";";
+                        // console.log(sql2)
+                        connection.query(sql2, function (err, results) {
+                            if (err) throw err;
+                        });
+                    } else {
+                        console.log("I'm sorry, we don't seem to have that much in stock right now.")
+                    };
+                    // console.log(chosenItem);node boom.js
+                }
+            };
+
+
+            var sql3 = "SELECT * from inventory;";
+            // console.log(sql2)
+            connection.query(sql3, function (err, results) {
+                if (err) throw err;
+
+                console.table(results);
+
+                againConfirm();
+            });
+        });
+    });
+}
--- boomSchema.sql
+++ boomSchema.sql
@@ -1,7 +1,10 @@
--- Create database
+
+
+DROP DATABASE IF EXISTS boomazon_DB;
+
 CREATE DATABASE boomazon_DB;
 -- for all the following commands, use this database
-USE boozamon_DB;
+USE boomazon_DB;
 
 -- creating a table for the auction
 CREATE TABLE inventory
@@ -10,16 +13,16 @@ CREATE TABLE inventory
   -- this will set the id(collumn 1) to auto, maining it will pick the next integer available--
   item_name VARCHAR(100) NOT NULL,
   -- this is setting the item_name as the title of the second collumn and it cannot be more than 1oo characters or be empty--
-  item_category VARCHAR(45) NOT NULL,
+  item_category VARCHAR(100) NOT NULL,
   -- this is setting the item_category as the title of the third collumn and it cannot be more than 45 characters or be empty--
-  item_price INT default 0,
+  item_price INT NOT NULL,
   -- this is setting the item_price as the title of the fourth collumn and the default integer value will start at 0--
-  item_stock INT default 0,
+  item_stock INT NOT NULL,
   -- this is setting the item_stock as the title of the fifth collumn and the default integer value will start at 0--
   PRIMARY KEY
     (id)
---this is setting the id as the promay key to navigate the table and al so organize it--
-)
+-- --this is setting the id as the promay key to navigate the table and al so organize it--
+);
 
 INSERT INTO inventory
     (item_name, item_category, item_price, item_stock)
@@ -40,24 +43,30 @@ INSERT INTO inventory
     (item_name, item_category, item_price, item_stock)
 VALUES    
     ("Necronomicon", "Ashley's Knife Knacks", 105, 1);
+
 INSERT INTO inventory
     (item_name, item_category, item_price, item_stock)
 VALUES
     ("55 Gallon Drum of Lube", "Rumors", 50, 15);
+
 INSERT INTO inventory
     (item_name, item_category, item_price, item_stock)
 VALUES
     ("Left Sock", "Gort's Lost Things", 2, 52);
+
 INSERT INTO inventory
     (item_name, item_category, item_price, item_stock)
 VALUES 
     ("Right Sock", "Gort's Lost Things", 6.75, 10);
+
 INSERT INTO inventory
     (item_name, item_category, item_price, item_stock)
 VALUES 
     ("Braaaaaiins", "You Don't Wanna Know", 20, 100);
+
 INSERT INTO inventory
     (item_name, item_category, item_price, item_stock)
 VALUES 
-("")   
+    ("Scottland's Freedom", "Sir William Wallace", 0, 5000000000000);
+
 ;
